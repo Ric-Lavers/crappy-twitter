332e20de532f790011dc0b34aa279f7e06af2699
Ric-Lavers <ric_lavers@outlook.com>
2017-09-30 12:57:57 +1000

    js row disappear

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,14 +16,37 @@
 
 //= require jquery3
 //= require popper
+
+//////////////first a
+// $(document).ready(function(){
+//   $(".box").click(function(){
+//     $(this).toggleClass("clear");
+//     //  setTimeout(function(){ $(".o").toggleClass("clear"); }, 400);
+//   });
+// });
+
 $(document).ready(function(){
-  $(".box").click(function(){
+  $(".box").click(function(element){
     $(this).toggleClass("clear");
+    // var txt = "";
+    // txt += "this: " + el;
+    el = element.currentTarget.attributes.id;
+    grid = $("div#jungletron.display-grid");
+    //  $(".box").html(el.toString());
+    console.log(grid);
+    for (var i = 1; i<12; i++){
+      offRow(i);
+    };
+
     //  setTimeout(function(){ $(".o").toggleClass("clear"); }, 400);
   });
 });
-
-
+function offRow(j){
+  setTimeout(function(){
+    $("div#a" + j + ".box").toggleClass("clear");
+    // $("div#b2.box").html(i);
+  }, 40 * j);
+}
 
 
 $(document).ready(function(){
